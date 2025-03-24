6573a787b4bebffa0d28bb66a827e7737feb0c07
JMBIGMAC <134791439+JMBIGMAC@users.noreply.github.com>
2025-03-24 11:06:24 +0000

    0.0.1

---
+++ dashborad/backend/expenses/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Expense
+from .serializers import ExpenseSerializer
+
+class ExpenseViewSet(viewsets.ModelViewSet):
+    queryset = Expense.objects.all().order_by('-created_at')
+    serializer_class = ExpenseSerializer
